782a193da0a6667f5ed34216e2dcff0ffc83101c
YoussefErian22 <134648200+YoussefErian22@users.noreply.github.com>
2023-06-09 15:07:13 +0300

    Delete lib directory

--- lib/common/images_paths/images_paths.dart
+++
@@ -1,5 +0,0 @@
-class ImagesPaths {
-  static const logo = 'images/common/appLogo.png';
-  static const animatedError = 'images/animated_error.gif';
-  static const noImage = 'images/no_image.png';
-}
--- lib/domain/product/profuct_type.dart
+++
@@ -1,7 +0,0 @@
-enum ProductEnum {
-  allItems,
-  plastic,
-  metal,
-  usedOil,
-  paper,
-}
